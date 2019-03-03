13b2ea1278bcd7a173cfa122d58dea69823f4610
jack <1537733595@qq.vom>
2019-03-03 13:05:35 +0800

    s

--- eledemo/src/components/haichen/shop/comment.vue
+++ eledemo/src/components/haichen/shop/comment.vue
@@ -1,44 +1,274 @@
 <template>
-  <div>
-    <div class="comment">
-      <div class="commentheader">
-        <div class="commentheaderleft">
-          <p>4.3</p>
+  <div class="commnents_html">
+    <div class="comments_body">
+      <header class="header" v-if="restaurant_data">
+        <div>
+          <p>{{restaurant_data.overall_score.toString().substr(0,3)}}</p>
           <p>综合评价</p>
-          <p>高于20%的商家</p>
+          <p>高于周边商家{{restaurant_data.compare_rating.toString().substr(2,2)}}%</p>
         </div>
-        <div class="commentheaderright">
-          <div>
+        <div>
+          <p>
             <span>服务态度</span>
-            <div>1</div>
-            <span class="score">4.2</span>
-          </div>
-          <div>
+            <el-rate
+              v-model="restaurant_data.service_score"
+              disabled
+              show-score
+              text-color="#ff9900"
+              score-template="{value}"
+            ></el-rate>
+          </p>
+          <p>
             <span>菜品评价</span>
-            <div>2</div>
-            <span class="score">4.2</span>
-          </div>
-          <div>
+            <el-rate
+              v-model="restaurant_data.food_score"
+              disabled
+              show-score
+              text-color="#ff9900"
+              score-template="{value}"
+            ></el-rate>
+          </p>
+          <p>
             <span>送达时间</span>
-            <div>3</div>
-            <span class="score2">4.3</span>
-          </div>
+            <span>{{restaurant_data.deliver_time}}分钟</span>
+          </p>
         </div>
-      </div>
-      <div class="trueComment">
-        <ul>
-          <li
-            :key="index"
-            class="li2"
-            v-for="(value,index) in comment"
-          >{{value.name}}({{value.count}})</li>
-        </ul>
-      </div>
+      </header>
+      <ul class="click">
+        <li
+          @click="change_color(index)"
+          :class="a==index?'liColor':'liColor2'"
+          :key="index"
+          v-for="(value,index) in restaurant_comments"
+        >{{value.name}}( {{value.count}})</li>
+      </ul>
+      <ul v-if="restaurant_comments_information" class="restaurant_comments_information">
+        <li class="detail" :key="index" v-for="(value,index) in restaurant_comments_information">
+          <img
+            :src="value.avatar==''?'https://elm.cangdu.org/img/default.jpg':'https://fuss10.elemecdn.com/'+value.avatar+'.jpeg'"
+          >
+          <section>
+            <header>
+              <section>
+                <p>{{value.username}}</p>
+                <div>
+                  <el-rate v-model="value.rating_star" disabled text-color="#ff9900"></el-rate>
+                  <span>{{value.time_spent_desc}}</span>
+                </div>
+              </section>
+              <time>{{value.rated_at}}</time>
+            </header>
+            <ul class="food_img_ul">
+              <li :key="index" v-for="(value,index) in value.item_ratings">
+                <img
+                  v-if="value.image_hash&&value.image_hash!=''"
+                  class="shop_image"
+                  :src="'https://fuss10.elemecdn.com/'+value.image_hash+'.jpeg'"
+                >
+              </li>
+            </ul>
+            <ul class="food_name_ul">
+              <li :key="index" v-for="(value,index) in value.item_ratings">{{value.food_name}}</li>
+            </ul>
+          </section>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      restaurant_id: null,
+      restaurant_data: null,
+      restaurant_comments: null,
+      restaurant_comments_information: null,
+      a: 0,
+      image_url: null
+    };
+  },
+  methods: {
+    change_color(index) {
+      this.a = index;
+    }
+  },
+  mounted() {
+    console.log(document.getElementsByClassName("el-rate__item")[0]);
+    // document.getElementsByClassName('el-rate')[0].style="float:left;"
+  },
+  updated() {
+    //  document.getElementsByClassName('el-rate')[0].style="float:left;"
+    console.log(document.getElementsByClassName("el-rate__item")[0]);
+  },
+  created() {
+    this.restaurant_id = this.$route.query.id;
+    this.$http({
+      method: "get",
+      url:
+        "https://elm.cangdu.org/ugc/v2/restaurants/" +
+        this.restaurant_id +
+        "/ratings/scores"
+    }).then(res => {});
+
+    this.$http({
+      method: "get",
+      url:
+        "https://elm.cangdu.org/ugc/v2/restaurants/" +
+        this.restaurant_id +
+        "/ratings/tags"
+    }).then(res => {
+      this.restaurant_comments = res.data;
+    });
+
+    this.$http({
+      method: "get",
+      url:
+        "https://elm.cangdu.org/ugc/v2/restaurants/" +
+        this.restaurant_id +
+        "/ratings"
+    }).then(res => {
+      //elm.cangdu.org/img/default.jpg
+      this.restaurant_comments_information = res.data;
+      console.log(this.restaurant_comments_information, "ssssss");
+    });
+  }
+};
 </script>
 <style scoped>
+.shop_image {
+  width: 0.7rem;
+}
+.commnents_html {
+  overflow: auto;
+}
+.comments_body {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  overflow: scroll;
+}
+.food_name_ul > li {
+  width: 0.5rem;
+  height: 0.2rem;
+  margin-right: 0.2rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  border: 0.01rem solid white;
+}
+.food_name_ul {
+  display: flex;
+}
+.food_img_ul {
+  display: flex;
+}
+.detail > section > header > section > div span {
+}
+.el-rate {
+  margin-left: -0.24rem;
+  transform: scale(0.5);
+}
+.detail > section > header > section > div {
+  display: flex;
+}
+.detail > section > header p {
+  margin-bottom: 0.05rem;
+}
+.detail > section > header {
+  display: flex;
+  justify-content: space-between;
+}
+.detail > section {
+  flex: 1;
+}
+.detail {
+  display: flex;
+  margin-bottom: 0.3rem;
+}
+.detail > img {
+  width: 0.3rem;
+  height: 0.3rem;
+  border-radius: 50%;
+  margin-right: 0.2rem;
+}
+.restaurant_comments_information {
+  box-sizing: border-box;
+  padding: 0.1rem 0.15rem;
+  flex: 1;
+  overflow: auto;
+}
+.liColor {
+  background: blue;
+  color: white;
+}
+.liColor2 {
+  background: #ebf5ff;
+  color: #6d7885;
+}
+.click > li {
+  font-size: 0.12rem;
+
+  padding: 0.05rem 0.05rem;
+  box-sizing: border-box;
+  border-radius: 0.04rem;
+  border: 1px;
+  margin: 0.05rem 0.07rem;
+}
+.click {
+  margin-top: 0.05rem;
+  margin-bottom: 0.05rem;
+  box-sizing: border-box;
+  padding: 0.1rem 0.15rem;
+  display: flex;
+  flex-wrap: wrap;
+  background-color: #fff;
+}
+.header > div:nth-child(2) > p > span {
+  font-size: 0.13rem;
+  flex: 3;
+}
+.header > div:nth-child(2) > p > div {
+  font-size: 0.13rem;
+  flex: 8;
+  overflow: hidden;
+}
+.el-rate {
+  transform: scale(0.6);
+}
+.header > div:nth-child(2) > p {
+  font-size: 0.13rem;
+  display: flex;
+}
+.header > div:nth-child(1) > p:nth-child(3) {
+  color: gray;
+  text-align: center;
+  font-size: 0.12rem;
+}
+.header > div:nth-child(1) > p:nth-child(2) {
+  color: gray;
+  text-align: center;
+  font-size: 0.17rem;
+}
+.header > div:nth-child(1) > p:nth-child(1) {
+  color: red;
+  text-align: center;
+  font-size: 0.25rem;
+}
+.header > div:nth-child(2) {
+  flex: 4;
+  box-sizing: border-box;
+}
+.header > div:nth-child(1) {
+  flex: 3;
+  box-sizing: border-box;
+}
+.header {
+  width: 100%;
+  display: flex;
+  box-sizing: border-box;
+  padding: 0.1rem 0.1rem;
+  background: white;
+  margin-top: 0.03rem;
+}
 </style>
--- eledemo/src/components/haichen/shop/shop.vue
+++ eledemo/src/components/haichen/shop/shop.vue
@@ -1,453 +1,100 @@
 <template>
-  <div class="body222222">
-    <transition name="d">
-      <router-view v-if="$route.path=='/shop/foodDetail'" height="100%"></router-view>
-    </transition>
+  <div  v-on:call_back="call">
+     <!-- 
+      specification mask
+     -->
+     <div v-if="spect" class="specification_mask">
 
-    <div height="100%" v-if="$route.path=='/shop'" class="content_wrap">
-      <div v-if="restaurant" style="height:100%" class="body1">
-        <div class="show2"></div>
-        <div class="top2">
-          <div>
-            <!-- <router-link to="###"> -->
-            <section :style="'background:url('+image+imag2+')'" class="header">
-              <div class="mask">
-                <div class="left">
-                  <img :src="image+imag2">
-                </div>
-                <div class="right">
-                  <div v-if="restaurant" class="right2">
-                    <h4>{{restaurant.name}}</h4>
-                    <p>商家配送／{{restaurant.order_lead_time}}分钟送达/{{restaurant.piecewise_agent_fee.tips}}</p>
-                    <p>公告:{{restaurant.promotion_info}}</p>
-                  </div>
-                </div>
-                <div v-if="restaurant.activities.length>0" class="footer">
-                  <div>
-                    <p>
-                      <span class="subtract">{{restaurant.activities[0]. icon_name}}</span>
-                      <span class="descript">{{restaurant.activities[0]. description}}</span>
-                    </p>
-                    <p>
-                      <span class="descript">{{restaurant.activities.length}}</span>
-                      <span>></span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </section>
-            <!-- </router-link> -->
-            <!-- <router-link class="leftA"> -->
-            <i class="el-icon-arrow-left leftA" v-on:click.stop="up22"></i>
-            <!-- </router-link> -->
-            <router-link class="rightA" to="/">></router-link>
-          </div>
-          <div class="ssll">
-            <div class="shopmenu">
-              <div class="oo" v-on:click.stop="xian">
-                <span>商品</span>
-              </div>
-              <div class="oo" @click.stop="xian">
-                <span>评价</span>
-              </div>
-            </div>
-          </div>
+     </div>
+    <router-view v-if="$route.name=='shop_detail'"></router-view>
+    <div class="body1" v-if="$route.name=='shop'">
+      <header1></header1>
+      <div class="option">
+        <div >
+          <span @click="changecolor(0)" :class="index==0?'blue':'none'">商品</span>
         </div>
-
-        <div class="comment">
-          <div class="commentheader">
-            <div class="commentheaderleft">
-              <p v-if="score">{{score.overall_score}}</p>
-              <p>综合评价</p>
-              <p v-if="score">高于{{score.truerate}}%的商家</p>
-            </div>
-            <div class="commentheaderright">
-              <div>
-                <span class="com_text">服务态度</span>
-                <div>
-                  <el-rate v-if="score" v-model="score.attitude" disabled text-color="#ff9900"></el-rate>
-                </div>
-                <span v-if="score" class="score">{{score.attitude}}</span>
-              </div>
-              <div>
-                <span class="com_text">菜品评价</span>
-                <div>
-                  <el-rate v-if="score" v-model="score.food_score" disabled text-color="#ff9900"></el-rate>
-                </div>
-                <span v-if="score" class="score">{{score.food_score}}</span>
-              </div>
-              <div>
-                <span class="com_text">送达时间</span>
-                <span v-if="score" class="score2">{{score.time}}分钟</span>
-              </div>
-            </div>
-          </div>
-          <div class="trueComment">
-            <ul>
-              <li
-                class="li2"
-                v-for="(value,index) in comment"
-                :key="index"
-              >{{value.name}}({{value.count}})</li>
-            </ul>
-          </div>
-        </div>
-        <div></div>
-        <div class="shop1">
-          <shop></shop>
+        <div>
+          <span @click="changecolor(1)" :class="index==1?'blue':'none'">评价</span>
         </div>
       </div>
+      <comment v-if="index==1"></comment>
+      <shopping v-else></shopping>
     </div>
   </div>
 </template>
 <script>
-import merchandise from "./merchandise.vue";
-import foodDetail from "./foodDetail.vue";
+import header1 from "./header";
+import comment from "./comment";
+import shopping from "./shopping";
 export default {
   data() {
     return {
-      comment: null,
-      restaurant: null,
-      image: "https://elm.cangdu.org/img/",
-      imag2: null,
-      score: null,
-      value5: 3.7,
-      ll: 1,
-      background222: "none",
-      transition: false,
-      restaurantId: null
+      index:0,
+     spect:null
     };
   },
-  components: {
-    shop: merchandise
-  },
   methods: {
-    up22() {
-      console.log("走了没");
-      this.$router.go(-1);
-    },
-    xian(even) {
-      this.ll = this.ll + 1;
-      let gg = document.getElementsByClassName("oo");
-      let gg1 = document.getElementsByClassName("comment")[0];
-      let gg2 = document.getElementsByClassName("shop1")[0];
-      for (let k = 0; k < gg.length; k++) {
-        if (even.currentTarget == gg[k]) {
-          gg[k].children[0].style =
-            "border-bottom:0.05rem solid blue;color:blue";
-          if (k == 0) {
-            gg2.style = "display:block";
-            gg1.style = "display:none";
-          } else {
-            gg2.style = "display:none";
-            gg1.style = "display:block";
-          }
-        } else {
-          gg[k].children[0].style = "border-bottom:0;color:black";
-        }
+      changecolor(index){
+        this.index=index;
+        console.log(this.index)
+      },
+      call(spect){
+        console.log("你哈")
+        this.spect=spect
       }
-    }
   },
-  created() {
-    this.restaurantId = this.$route.query.id;
-    console.log("餐馆id", this.restaurantId);
-    let header2 = document.getElementsByClassName("header");
-    console.log(this.$route);
-    this.$http({
-      url:
-        "https://elm.cangdu.org/ugc/v2/restaurants/" +
-        this.restaurantId +
-        "/ratings/tags",
-      method: "get"
-    }).then(res => {
-      this.comment = res.data;
-      console.log(res, "111111111111111111111");
-    });
-
-    this.$http({
-      url: "https://elm.cangdu.org/shopping/restaurant/" + this.restaurantId,
-      method: "get"
-    }).then(res => {
-      this.restaurant = res.data;
-      this.imag2 = res.data.image_path;
-      console.log(res, "是是是是是是所所所所所所所所所所所");
-    });
-
-    this.$http({
-      url:
-        "https://elm.cangdu.org/ugc/v2/restaurants/" +
-        this.restaurantId +
-        "/ratings/scores",
-      method: "get"
-    }).then(res => {
-      console.log("状态",res)
-      // let attitude1 = res&&res.data.service_score.toString().substr(0, 3);
-      // let food_score1 = res.data&&res.data.food_score.toString().substr(0, 3);
-      // let rate = res.data&&res.data.compare_rating.toString().substr(2, 3);
-      // let rate2 = rate.substr(2, 1);
-      // let rate3 = rate.substr(0, 2);
-      // let time = res.data&&res.data.deliver_time.toString();
-      // let overall_score = res.data&&res.data.overall_score.toString().substr(0, 3);
-
-      // let truerate = rate3 + "." + rate2;
-
-      // let attitude = parseFloat(attitude1);
-      // let food_score = parseFloat(food_score1);
-      // this.score = {
-      //   attitude: attitude,
-      //   food_score: food_score,
-      //   truerate: truerate,
-      //   time: time,
-      //   overall_score: overall_score
-      // };
-    });
+  components: {
+    header1,
+    comment,
+    shopping,
   },
-  mounted() {
-    console.log("mounted");
-    let a1 = document.getElementsByClassName("el-rate__icon");
-    console.log(a1[0]);
-    for (let b = 0; b < a1.length; b++) {
-      a1[b].style.fontSize = "0.12rem";
-    }
-    let a2 = document.getElementsByClassName(" el-rate__text");
-
-    for (let b1 = 0; b1 < a2.length; b1++) {
-      a2[b1].style.display = "none";
-    }
-
-    let a3 = document.getElementsByClassName("el-rate__item");
-
-    for (let b3 = 0; b3 < a3.length; b3++) {
-      a3[b3].style.width = "0.12rem";
-    }
-  },
-  beforeUpdate() {
-    let a1 = document.getElementsByClassName("el-rate__icon");
-    console.log("upupupu");
-    for (let b = 0; b < a1.length; b++) {
-      a1[b].style.fontSize = "0.12rem";
-    }
-
-    let a2 = document.getElementsByClassName(" el-rate__text");
-
-    for (let b1 = 0; b1 < a2.length; b1++) {
-      a2[b1].style.display = "none";
-    }
-
-    let a3 = document.getElementsByClassName("el-rate__item");
-
-    for (let b3 = 0; b3 < a3.length; b3++) {
-      a3[b3].style.width = "0.12rem";
-    }
-  }
+  created() {}
 };
 </script>
 <style scoped>
-.body222222 {
-  margin-top: 1.6rem;
-  height: 11rem;
-}
-.content_wrap {
-  height: 11rem;
-}
-.comment {
-  margin-top: -11%;
-  background: #fff;
-}
-.shop1 {
-  margin-top: -8%;
-  height: 100%;
-}
-.ssll {
-  background: white;
-}
-.top2 {
+
+.specification_mask{
   position: fixed;
   top: 0;
-  left: 0;
-  width: 100%;
-  z-index: 10000;
-}
-#shopping {
-  position: fixed;
   bottom: 0;
   left: 0;
+  right: 0;
+  background:rgba(0, 0, 0, 0.4);
+  z-index:400
 }
-.show2 {
-  width: 100%;
-  height: 100%;
+.body1{
+  display: flex;
   position: absolute;
   top: 0;
   left: 0;
-  z-index: 5000;
-  background: rgba(0, 0, 0, 0.3);
-  display: none;
-}
-.el-rate {
-  transform: scale(0.5);
-}
-.mask {
-  height: 0.7rem;
-  padding: 0.09375rem 0 0.09375rem 0.09375rem;
-  background: rgba(119, 103, 137, 0.43);
-}
-.footer span {
-  color: white;
-}
-.footer > div {
-  display: flex;
-  padding-right: 0.05rem;
-  justify-content: space-between;
-}
-.descript {
-  color: white;
-  font-size: 0.009rem;
-}
-.descript {
-  color: white;
-  font-size: 0.009rem;
-}
-.subtract {
-  background-color: rgb(240, 115, 115);
-  border-color: rgb(240, 115, 115);
-  color: white;
-}
-.comment {
-  display: none;
-}
-.li2 {
-  float: left;
-  height: 0.19rem;
-  padding: 0.07031rem 0.07031rem;
-  background: #ebf5ff;
-  font-size: 0.14rem;
-  text-align: center;
-  line-height: 0.19rem;
-  margin-right: 0.09375rem;
-  margin-bottom: 0.04688rem;
-}
-.trueComment {
-  overflow: hidden;
-  padding: 0.11719rem 0.11719rem;
-}
-.score2 {
-  color: #999;
-  font-size: 0.16rem;
-}
-.score {
-  font-size: 0.16rem;
-  color: #f60;
-}
-.commentheaderright {
-  display: flex;
-  justify-content: space-between;
-}
-.commentheaderright .com_text {
-  width: 0.6rem;
-  font-size: 0.12rem;
-}
-.commentheaderright > p > div {
-  width: 0.57938rem;
-}
-.commentheaderright > div {
-  display: flex;
-  justify-content: space-between;
-}
-.commentheaderleft > p:nth-child(1) {
-  font-size: 0.3rem;
-  color: #f60;
-}
-.commentheaderleft > p:nth-child(3) {
-  font-size: 0.14rem;
-}
-.commentheader > div {
-  width: 50%;
-  height: 0.72344rem;
-  box-sizing: border-box;
-  display: flex;
+  right: 0;
+  bottom: 0;
   flex-direction: column;
+}
+.blue{
+   color:blue;
+   border-bottom: 0.02rem solid blue;
+  
+}
+.none{
+   color:black;
+   
+   border-bottom: 0px;
+}
+.option > div {
+  flex: 1;
   text-align: center;
-  justify-content: space-between;
-}
-.commentheader {
-  padding: 0.1875rem 0.11719rem;
-  margin-bottom: 0.11719rem;
-  border-bottom: 0.14rem solid rgba(119, 103, 137, 0.43);
-  display: flex;
-  justify-content: space-between;
-}
-.shopmenu > div {
-  width: 50%;
+  padding-top: 0.15rem;
+  padding-bottom: 0.15rem;
   box-sizing: border-box;
-  height: 0.31rem;
-  text-align: center;
-  line-height: 0.31rem;
-  font-size: 0.16rem;
-  margin-top: -0.05rem;
+  font-weight: 1000;
+  font-size: 0.15rem;
 }
-.shopmenu {
-  padding: 0.07031rem 0.14063rem;
-  /* height: 0.31rem; */
-  display: flex;
-  border-bottom: 1px solid gray;
-  justify-content: space-between;
-}
-.rightA {
-  color: white;
-  font-size: 0.3rem;
-  font-weight: 1700;
-  position: absolute;
-  top: 0.26rem;
-  right: 0.02rem;
-}
-.leftA {
-  color: white;
-  font-size: 0.3rem;
-  font-weight: 1700;
-  position: absolute;
-  top: 0.01rem;
-}
-.right2 {
-  display: flex;
-  flex-direction: column;
-  height: 0.71063rem;
-  justify-content: space-between;
-}
-.right h4 {
-  color: white;
-  font-size: 0.2rem;
-}
-.right p {
-  color: white;
-  font-size: 0.12rem;
-}
-.left img {
-  width: 0.67969rem;
-  height: 0.67969rem;
-}
-.right {
-  width: 79.48717%;
-  height: 0.71063rem;
-  float: right;
-}
-.left {
-  width: 18.5898%;
-  height: 0.71063rem;
-
-  float: left;
-}
-.footer {
-  margin-top: 0.15rem;
-  width: 100%;
-  height: 0.17156rem;
-  background: red;
-  float: left;
-}
-.header {
-  height: 0.8981rem;
-  background: red;
-}
-.body1 {
-  position: relative;
+.option {
+  display:-webkit-box;
+  /* justify-content: space-between;
+  flex-direction: row; */
+  background: white;
 }
 </style>
+
